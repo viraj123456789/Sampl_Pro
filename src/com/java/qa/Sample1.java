aeacb310dd1191d58ba6c6c324b20888fc217b6e
unknown <virajmemathi@gmail.com>
2023-01-11 15:27:04 +0530

    this is my 1st push from gitbash

---
+++ src/com/java/qa/Sample1.java
@@ -0,0 +1,10 @@
+package com.java.qa;
+
+public class Sample1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("this is sample project");	
+	}
+
+}
---
+++ src/com/java/qa/Sample2.java
@@ -0,0 +1,11 @@
+package com.java.qa;
+
+public class Sample2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("This is Sample 2");
+		
+	}
+
+}
